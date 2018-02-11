5b3b896a55708e3b02ac8e5f93d54df233dd21ff
AlbertHuyb <13935047516@163.com>
2018-02-11 11:34:09 +0800

    students id

--- backend/urls.py
+++ backend/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 app_name = 'backend'
 urlpatterns = [
-    path('teams/add', views.TeamAdd),
-    path('teams/join',views.TeamJoin),
+    path('teams/add/', views.TeamAdd),
+    path('teams/join/',views.TeamJoin),
     path('team_id=<int:pk>/', views.GroupDetail.as_view(), name='GroupDetail'),
     path('user_id=<int:pk>/', views.StudentDetail.as_view(), name='StudentDetail'),
     path('RuleFile_id=<int:pk>', views.RuleFileView.as_view(), name='RuleFile'),
