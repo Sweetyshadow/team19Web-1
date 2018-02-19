a6ee8ca11da180e524f87492faadac305a18db84
AlbertHuyb <13935047516@163.com>
2018-02-19 16:59:02 +0800

    modify

--- backend/forms.py
+++ backend/forms.py
@@ -3,10 +3,10 @@ from django import forms
 class StudentRegForm(forms.Form):
 	name = forms.CharField(label='name',max_length = 20)
 	password = forms.CharField(label='pwd',max_length = 20, min_length = 8)
-	email forms.EmailField()
+	email = forms.EmailField()
 
 class StudentLoginForm(forms.Form):
 	name = forms.CharField(label='name', max_length = 20)
 	password = forms.CharField(label='pwd', max_length = 20, min_length = 8)
-	
+
     
