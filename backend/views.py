3b8a7ffbbf00584ef29be36578332616f0540de6
AlbertHuyb <13935047516@163.com>
2018-02-19 17:12:59 +0800

    modify

--- backend/views.py
+++ backend/views.py
@@ -42,7 +42,7 @@ def StudentReg(request):
         success = True
         message = ""
         form = StudentRegForm(request.POST)
-        return JsonResponse(str(form))
+        return JsonResponse({'response':str(form)})
         if form.is_valid():
             the_name = form.cleaned_data['name']
             the_pwd = form.cleaned_data['pwd']
