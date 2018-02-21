11fbc55c290a45b3a65ea32b3decb1e5edf73442
AlbertHuyb <13935047516@163.com>
2018-02-21 16:49:36 +0800

    salt

--- backend/views.py
+++ backend/views.py
@@ -58,7 +58,7 @@ def StudentReg(request):
                 success = True
             if success == True:
                 the_salt = binascii.hexlify(os.urandom(4)).decode(),
-                return JsonResponse({'message':str(the_salt)})
+                return JsonResponse({'message':the_salt[0]})
                 new_student = StudentInfo.objects.create(
                     student_nickname = the_name,
                     salt = the_salt,
