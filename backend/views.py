c48778175c50eb6c5f693f8555f03b55fe135081
AlbertHuyb <13935047516@163.com>
2018-02-21 13:08:49 +0800

    form

--- backend/views.py
+++ backend/views.py
@@ -86,7 +86,7 @@ def StudentLogin(request):
                 if the_student.password == the_pwd:
                     success = True
                 else :
-                    message += "wrong password!" + the_student.password + the_pwd
+                    message += "wrong password!"
             except:
                 message += "the user does not exist!"
 
