ef36fb5bcca3a016874e481d72907425f4535ae6
AlbertHuyb <13935047516@163.com>
2018-03-14 23:19:08 +0800

    实现轮询

--- backend/urls.py
+++ backend/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('teams/allteam/',views.AllTeam),
     path('teams/score/',views.GetScore),
     path('teams/battle/',views.Battle),
+    path('teams/inquire/<int:id1>+<int:id2>',views.Inquire),
     path('team_id=<int:pk>/', views.GroupDetail.as_view(), name='GroupDetail'),
     path('user_id=<int:pk>/', views.StudentDetail.as_view(), name='StudentDetail'),
     path('RuleFile_id=<int:pk>', views.RuleFileView.as_view(), name='RuleFile'),
