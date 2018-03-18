f38fe6c9ec20884efe360ec69601e8296e4bce0a
Iris Li <461729541@qq.com>
2018-03-18 15:19:11 +0800

    add footer

--- TeamStyle19_front/src/components/home.vue
+++ TeamStyle19_front/src/components/home.vue
@@ -19,6 +19,32 @@
     >
     </vue-particles>
     <h1> Teamstyle19</h1>
+    <el-footer>
+      <div class="column">
+        <div class="left">
+          <p>联系我们</p>
+        </div>
+        <div class="right">
+          <p><a>EESAST</a></p>
+          <p><a href="https://github.com/eesast/TeamStyle19-BUG-Report/issues/new">BUG反馈</a></p>
+        </div>
+      </div>
+      <div class="column">
+        <div class="left">
+          <p>技术支持</p>
+        </div>
+        <div class="right">
+          <el-tag><a href="https://www.djangoproject.com/">Django</a></el-tag>
+          <el-tag><a href="https://www.docker.com/">Docker</a></el-tag>
+          <el-tag><a href="https://cn.vuejs.org/">Vue</a></el-tag>
+          <el-tag><a href="element.eleme.io/">ElementUI</a></el-tag>
+          <el-tag><a href="https://github.com/">GitHub</a></el-tag>
+        </div>
+      </div>
+      <div class="column">
+        <img id="logo" src="../../static/img/logo.jpg">
+      </div>
+    </el-footer>
   </div>
 </template>
 
@@ -57,5 +83,28 @@ export default {
     font-family: 'Megrim', cursive;
     font-size: 64px;
     cursor: default;
+  }
+  .column{
+    display: inline-block;
+    width: 33%;
+    vertical-align: middle;
+  }
+  .column .left{
+    display:inline-block;
+    width: 35%;
+    vertical-align: top;
+  }
+  .column .right{
+    display: inline-block;
+    width: 60%;
+    color:gray;
+    font-size: 80%;
+  }
+  a,a:hover,a:visited,a:link,a:active{
+    text-decoration: none;
+    color: inherit;
+  }
+  #logo{
+    width: 100%;
   }
 </style>
