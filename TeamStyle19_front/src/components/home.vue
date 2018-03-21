d04c4e6a1c8107f2f4bad5769151755265871405
Sweetyshadow <Shadow_8890@126.com>
2018-03-21 23:20:18 +0800

    footer modified

--- TeamStyle19_front/src/components/home.vue
+++ TeamStyle19_front/src/components/home.vue
@@ -19,14 +19,36 @@
     >
     </vue-particles>
     <h1> Teamstyle19</h1>
-    <footer>
+    <div class="footer">
       <div class="col left">
+        <h4> 联系我们</h4>
+        <p><a> EESAST</a><p>
+        <p><a href="https://github.com/eesast/TeamStyle19-BUG-Report/issues/new">问题反馈</a></p>
       </div>
+      
       <div class="col mid">
+        <img src="/static/img/logo_main_metal.png"/>
       </div>
+            
       <div class="col right">
+        <h4>技术支持</h4>
+        <a href="https://www.djangoproject.com/">
+          <img src="/static/img/django.svg" />
+        </a>
+        <a href="https://www.docker.com/">
+          <img src="/static/img/docker.svg"/>
+        </a>
+        <a href="https://cn.vuejs.org/">
+          <img src="static/img/vue.png"/>
+        </a>
+        <a href="element.eleme.io/">
+          <img src="/static/img/elementui.svg"/>
+        </a>
+        <a href="https://github.com/">
+          <img src="/static/img/github.svg"/>
+        </a>
       </div>
-    </footer>
+    </div>
     <!--el-footer>
       <div class="column">
         <div class="left">
@@ -73,12 +95,13 @@ export default {
   }
 }
 </script>
-<style scoped>
+<style lang="scss" scoped>
   #particles{
     height: 100vh;
     position:absolute;
     top: 0;
     width: 100%;
+    border-bottom: solid #f1f1f1 2px;
   }
   #particles-js {
     height: 100%;
@@ -93,22 +116,52 @@ export default {
     font-size: 64px;
     cursor: default;
   }
-  .column{
-    display: inline-block;
-    width: 33%;
+  .footer{
+    width: 100%;
+    height: 150px;
+    display: flex;
+    flex-direction: row;
+  }
+  .col{
+    margin: 0;
     vertical-align: middle;
   }
-  .column .left{
-    display:inline-block;
+  .left{
+    order: 1;
     width: 35%;
-    vertical-align: top;
+    vertical-align: middle;
+    text-align: center;
+    align-items: flex-start;
+    a {
+      font-size: 14px;
+    }
   }
-  .column .right{
-    display: inline-block;
-    width: 60%;
-    color:gray;
-    font-size: 80%;
+  .mid{
+    order: 2;
+    width: 30%;
+    vertical-align: middle;
+    display: flex;
+    img {
+      margin: auto;
+      width: 100%;
+      align-items: center;
+    }
   }
+  .right {
+    order: 3;
+    width: 35%;
+    text-align: center;
+    align-items: flex-start;
+    img {
+      width: 30px;
+      height: 30px;
+    }
+    h4 {
+      margin-bottom: 30px;
+    }
+  }
+
+  
   a,a:hover,a:visited,a:link,a:active{
     text-decoration: none;
     color: inherit;
