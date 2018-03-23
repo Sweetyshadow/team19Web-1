c05495bd297eafefa8254b4bbc898d18eefa4815
AlbertHuyb <13935047516@163.com>
2018-03-23 22:23:09 +0800

    添加基础分数50

--- backend/models.py
+++ backend/models.py
@@ -18,7 +18,7 @@ class TeamInfo(models.Model):
     battle_time = models.IntegerField(default = 1)
     codes = models.TextField(null=True)
     history = models.CharField(max_length = 8000, default = '[]')
-    score = models.CharField(max_length = 800, default = '[]')
+    score = models.CharField(max_length = 800, default = '[{"score":50}]')
 
     def __str__(self):
         return self.team_name
