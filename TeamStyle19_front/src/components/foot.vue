d637edbb6a2b128730f14e71fa93fadb02e4603b
Sweetyshadow <Shadow_8890@126.com>
2018-03-23 16:41:53 +0800

    页脚响应式设计

--- TeamStyle19_front/src/components/foot.vue
+++ TeamStyle19_front/src/components/foot.vue
@@ -100,4 +100,24 @@ a,a:active,a:link,a:hover,a:visited{
       border-bottom: solid #f0f0f0 1px;
     }
   }
+@media screen and(max-width:720px) {
+  .footer{
+    flex-direction: column;
+    height:auto;
+  }
+  .col {
+    width: 100vw;
+    box-sizing: border-box;
+  }
+  .mid{
+    height: 100px;
+    img {
+      height: 100%;
+      width: auto;
+    }
+  }
+  .right{
+    margin-bottom: 30px;
+  }
+}
 </style>
