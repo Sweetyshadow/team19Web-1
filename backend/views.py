ece29c1cae428f42589d81b6d185834d0b95c82f
AlbertHuyb <13935047516@163.com>
2018-03-24 14:58:43 +0800

    平局计分修改

--- backend/views.py
+++ backend/views.py
@@ -382,7 +382,7 @@ def AllTeam(request):
                 'teamid':team.id,
                 'teamname':team.team_name,
                 'scale':team.member_num,
-                'score':team.get_score()[-1]['score']
+                'score':team.get_score()[-1]['score'],
                 'leader':team.leader,
                 'hasAI':hasAI,
                 'member1':team.member1,
@@ -702,6 +702,8 @@ def Inquire(request,battleid):
                 winner = team2.team_name
                 loser = team1.team_name
             elif response['result'] == '2':
+                score1 = score1 + 32 * (0.5 - E1)
+                score2 = score2 + 32 * (0.5 - E2)
                 score_1 = round(score1,2)
                 score_2 = round(score2,2)
                 winner = 'none'
